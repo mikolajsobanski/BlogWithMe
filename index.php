39748a29e10bf13fa54f3d11aa1fcfd57d6e055c
Mikolaj Sobanski <mikolajsobanski01@gmail.com>
2022-10-28 11:21:37 +0200

    Base html with php routing

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 
-echo 'Hi there 👋';
-echo 'no co tam';
-echo 'no co tam';
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url( $path, PHP_URL_PATH);
+
+Router::get('', 'DefaultController');
+Router::get('home', 'DefaultController');
+
+Router::run($path);
